1e0ff0c944a3e03799a76e1a8f699d3d90b0234e
cmdwhoami <30704361+cmdwhoami@users.noreply.github.com>
2021-08-16 21:54:06 -0400

    Update set_time.sh

--- SDcard/set_time.sh
+++ SDcard/set_time.sh
@@ -13,21 +13,22 @@
 ## Decription: Sets system time using user defined 
 ##             NTP server & timezone string.
 
-##################################################
-## NTP Server IP        # Edit your server IP here
-#==================================================
+####################################################
+## NTP Server IP	# Edit your server IP here
+#===================================================
 TIME_SERVER="192.168.10.1"
+#===================================================
 
-#################################################
-## TimeZone Offset      # Uncomment your timezone
-#==================================================
-TIMEZONE="EST-5EDT-4,M3.2.0/02:00:00,M11.1.0/02:00:00"      #Eastern
-#TIMEZONE="CST-6CDT-5,M3.2.0/02:00:00,M11.1.0/02:00:00"      #Central
-#TIMEZONE="MST-7MDT-6,M3.2.0/02:00:00,M11.1.0/02:00:00"      #Mountian
-#TIMEZONE="PST-8PDT-7,M3.2.0/02:00:00,M11.1.0/02:00:00"      #Pacific
-#TIMEZONE="ASKT-9AKDT-8,M3.2.0/02:00:00,M11.1.0/02:00:00"    #Alaska
-#TIMEZONE="HST-11HDT-10,M3.2.0/02:00:00,M11.1.0/02:00:00"    #Hawaii
-#==================================================
+####################################################
+## TimeZone Offset	# Uncomment your timezone
+#===================================================
+TIMEZONE="EST-5EDT-4,M3.2.0/02:00:00,M11.1.0/02:00:00"		#Eastern
+#TIMEZONE="CST-6CDT-5,M3.2.0/02:00:00,M11.1.0/02:00:00"		#Central
+#TIMEZONE="MST-7MDT-6,M3.2.0/02:00:00,M11.1.0/02:00:00"		#Mountian
+#TIMEZONE="PST-8PDT-7,M3.2.0/02:00:00,M11.1.0/02:00:00"		#Pacific
+#TIMEZONE="ASKT-9AKDT-8,M3.2.0/02:00:00,M11.1.0/02:00:00"	#Alaska
+#TIMEZONE="HST-11HDT-10,M3.2.0/02:00:00,M11.1.0/02:00:00"	#Hawaii
+#===================================================
 
 # Sync with local ntp server
 
