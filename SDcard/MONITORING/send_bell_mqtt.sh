1bf6e2d7825960d73a56a2f20101884f9f76df4b
whoamiTM <30704361+whoamiTM@users.noreply.github.com>
2023-07-29 20:52:24 -0400

    Update send_bell_mqtt.sh
    Sends Image over MQTT on button press of SD-M5 Doorbell

--- SDcard/MONITORING/send_bell_mqtt.sh
+++ SDcard/MONITORING/send_bell_mqtt.sh
@@ -1,10 +1,40 @@
 #!/bin/sh
 
-# This example publishes two MQTT messages for DOMOTICZ Home Automation system
-# Fill in your MQTT broker's IP/Port number and topic/message
+###############################################
+##  __  __  ___ _____ _____ ___ _   _ ___    ##
+##  |  \/  |/ _ \_   _|_   _| _ \ | | | _ )  ##
+##  | |\/| | (_) || |   | | |  _/ |_| | _ \  ##
+##  |_|  |_|\__\_\|_|   |_|_|_|  \___/|___/  ##
+##                    |___|                  ##
+##                                           ##
+###############################################
 
-/mnt/disc1/MONITORING/mosquitto_pub -h 192.168.1.3 -p 1883  -t "domoticz/in" -m '{"command":"switchlight", 
-"idx" : 79, "switchcmd": "On"}'
-/mnt/disc1/MONITORING/mosquitto_pub -h 192.168.1.3 -p 1883  -t "domoticz/in" -m '{"command":"switchlight", 
-"idx" : 119, "switchcmd": "On"}'
 
+#Publish MQTT
+
+# Today's date - such dir should be created for alarm pics
+DIR=`date +%Y-%m-%d`
+
+# Alarm picture storage
+PTH="/mnt/disc1/npc/push/alarm"
+
+# Wait until snapshot is taken
+/mnt/disc1/busybox sleep 5
+
+# Find latest image in the current date folder
+LATEST=`ls -t ${PTH}/${DIR} | /mnt/disc1/busybox head -1`
+
+# Full filename with path
+FILE=${PTH}/${DIR}/${LATEST}
+
+# Username for MQTT
+USER="your_username"
+
+# Password for MQTT
+PSSWD="your_password"
+
+# Send latest image
+/mnt/disc1/MONITORING/mosquitto_pub -h 192.168.2.202 -p 1883 -u ${USER} -P ${PSSWD} -t doorbell/push/image -f ${FILE}
+
+
+exit 0
